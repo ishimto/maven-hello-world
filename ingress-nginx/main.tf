dd4096d6b0a68d0f42e3a96e5f48492ffff205cd
shoham.bozorgi <shoham.buzorgi@gmail.com>
2025-05-07 00:57:11 +0300

    ingress controller nginx

---
+++ ingress-nginx/main.tf
@@ -0,0 +1,34 @@
+resource "helm_release" "nginx_ingress" {
+  name       = "ingress-nginx"
+  namespace  = "ingress-nginx"
+  chart      = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  version    = "4.10.1"
+  create_namespace = true
+
+  set {
+    name  = "controller.service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "controller.ingressClassResource.name"
+    value = "nginx"
+  }
+
+  set {
+    name  = "controller.ingressClassResource.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "controller.ingressClassResource.default"
+    value = "false"
+  }
+
+  set {
+    name  = "controller.ingressClass"
+    value = "nginx"
+  }
+}
+
