18a0484c09ab42e7858d7bccb6229949ce44ca29
shoham.bozorgi <shoham.buzorgi@gmail.com>
2025-05-06 21:22:09 +0300

    Revert "test if it will fail and not deploy"
    This reverts commit 4b02205b2dc179b134bf83248a600329f5f67098.

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "ttShoham wants to say Hello World!!!" );
+        System.out.println( "Shoham wants to say Hello World!!!" );
     }
 }
