cacf12e22b3b4516f8fb79ea9b987986405ddde3
shoham.bozorgi <shoham.buzorgi@gmail.com>
2025-05-06 20:45:59 +0300

    Revert "check if the tests fail as expected"
    This reverts commit 9e06e33528c0cb495e056346d38034cd96167be9.

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "ttttdfgdShoham wants to say Hello World!!!" );
+        System.out.println( "Shoham wants to say Hello World!!!" );
     }
 }
